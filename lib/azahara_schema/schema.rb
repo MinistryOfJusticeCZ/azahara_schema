cd6eb4813d19843dc280b82354df4d749a106540
Ondřej Ezr <oezr@msp.justice.cz>
2017-11-01 15:52:05 +0100

    quickfix for polymorphic associations

--- lib/azahara_schema/schema.rb
+++ lib/azahara_schema/schema.rb
@@ -156,7 +156,10 @@ module AzaharaSchema
 
     def available_associations
       @available_associations ||= model.reflect_on_all_associations.select do |association|
-        association.klass != model && !association_path.include?( association.name.to_s.singularize.to_sym ) && !association_path.include?( association.name.to_s.pluralize.to_sym )
+        association.klass != model &&
+          !association.options[:polymorphic] &&
+          !association_path.include?( association.name.to_s.singularize.to_sym ) &&
+          !association_path.include?( association.name.to_s.pluralize.to_sym )
       end.collect do |association|
         AzaharaSchema::Schema.schema_for(association.klass, parent_schema: self, association: association)
       end
